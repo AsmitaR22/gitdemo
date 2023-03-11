190d1e436de9e30bfc9bda72fca4a9b54f605524
AsmitaR22 <asmitaraut2212@gmail.com>
2023-03-11 12:43:08 +0530

    Asmita commit for automation

--- src/test/java/git_abc/Demo1.java
+++ src/test/java/git_abc/Demo1.java
@@ -1,13 +1,29 @@
 package git_abc;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Demo1 {
+public class Demo1 extends Demo2  {
+	
+	
+	
 	@Test
-	public void name() {
-		System.out.println("github");
+	public void test1() {
+		WebElement user = driver.findElement(By.id("user-name"));
+		Assert.assertTrue(user.isDisplayed());
+		user.sendKeys("Admin");
+		WebElement pass = driver.findElement(By.id("password"));
+		Assert.assertTrue(pass.isDisplayed());
+		pass.sendKeys("admin123");
+		WebElement button = driver.findElement(By.id("login-button"));
+		Assert.assertTrue(button.isDisplayed());
+		button.click();
+		WebElement page = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[1]/div[2]/div"));
+		Assert.assertTrue(page.isDisplayed());
 	}
-
 	
 
 }
--- src/test/java/git_abc/Demo2.java
+++ src/test/java/git_abc/Demo2.java
@@ -1,12 +1,28 @@
 package git_abc;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Demo2 {
-    @Test
-	public void abc() {
-	System.out.println("SARIKA");
-
+	WebDriver driver;
+	
+    @BeforeClass
+	void before() throws InterruptedException {
+        driver = new ChromeDriver();
+		driver.manage().window().fullscreen();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(3000);
+		Assert.assertEquals(driver.getTitle(),"OrangeHRM");	
 	}
+    
+    @AfterClass
+    void after() throws InterruptedException {
+    	Thread.sleep(2000);
+    	driver.close();
+    }
 
 }
